34e1333b1951f961512d74745379a73a4d374d64
cinth <votacinthia@gmail.com>
2024-11-23 16:13:35 -0300

    add bycript in login and register

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,18 +1,26 @@
 import * as userService from "../services/userService.js";
-import User from '../models/User.js'
+import User from '../models/User.js';
+import bcrypt from 'bcrypt'; // Importa bcrypt
 
-
-
+// Login de usuario
 export const loginUser = async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const user = await User.findOne({ username, password });
+    // Busca al usuario por nombre de usuario
+    const user = await User.findOne({ username });
     if (!user) {
-      return res.status(401).send("Credenciales incorrectas");
+      return res.status(401).send("Usuario incorrecto");
     }
-    req.session.user = user;
 
+    // Compara la contraseña proporcionada con la almacenada
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).send("Contraseña incorrecta");
+    }
+
+    // Almacena el usuario en la sesión si las credenciales son correctas
+    req.session.user = user;
     res.redirect("/main");
   } catch (error) {
     console.error("Error al autenticar el usuario:", error);
@@ -20,11 +28,7 @@ export const loginUser = async (req, res) => {
   }
 };
 
-
-
-
-
-// Logout de usuario 
+// Logout de usuario
 export const logoutUser = (req, res) => {
   req.session.destroy((err) => {
     if (err) return res.status(500).send("Error al cerrar sesión");
@@ -32,20 +36,26 @@ export const logoutUser = (req, res) => {
   });
 };
 
+// Crear un nuevo usuario
 export async function crearUsuario(req, res) {
   try {
     // Extraer los datos del usuario desde el cuerpo de la solicitud
     const { nombre, apellido, username, rol, password } = req.body;
 
-    // Crear un nuevo objeto con los datos proporcionados
+    // Encriptar la contraseña con bcrypt
+    const salt = await bcrypt.genSalt(10); // Genera un "salt" para agregar aleatoriedad
+    const hashedPassword = await bcrypt.hash(password, salt); // Genera el hash
+
+    // Crear un nuevo objeto con la contraseña encriptada
     const nuevoUsuario = {
       nombre,
       apellido,
       username,
       rol,
-      password, // todo encriptar la contraseña
+      password: hashedPassword, // Almacena la contraseña encriptada
     };
 
+    // Llama al servicio para guardar el usuario
     const usuarioCreado = await userService.crearUsuario(nuevoUsuario);
 
     return res.status(201).json(usuarioCreado);
@@ -56,6 +66,7 @@ export async function crearUsuario(req, res) {
         .status(error.statusCode || 400)
         .json({ message: error.message });
     }
-    return res.status(500).json({ message: MSG_ERROR_500 });
+    //return res.status(500).json({ message: MSG_ERROR_500 });
+    return res.status(500).json({ message: error.message });
   }
 }
